2f6c8673537e55a40a28b7af4540e15d626640b1
royhe <roy@poprx.ca>
2017-06-17 22:06:42 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//Swiper setup
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+//End of Swiper setup    
+
+
